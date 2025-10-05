55a02fb997741eeed721d19c2a2fc86a18065be0
Bryantrejos.h <bryantrejos.h@gmail.com>
2025-10-06 01:54:11 +0200

    update

--- T1/untitled/src/Main.java
+++ T1/untitled/src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+
+    // esto es un comentario
+    // otra linea de comentario
+    /*
+    linea de comentarios abiertos en los que puedo hacer las anotaciones privadas que quiera yo
+    y se puede dividir en varias lineas
+     */
+    //TODO me falta lo de abajo
+
+    public static void main(String[] args){
+        System.out.print("Este es mi primer programa en java\n\n");
+        System.out.println("Este es la segunda linea del probrama");
+        System.out.println("Esta es\nla tercera linea y estoy probando el salto de la n y del printnl a la vez dentro del parentesis");
+
+    }
+
+    public void nombreMetodo () {
+
+    }
+
+    //TODO prueba dos de un todo
 }
