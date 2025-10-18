9800432957902325319f4c8f806b657d04008bd8
Bryantrejos.h <bryantrejos.h@gmail.com>
2025-10-18 19:22:44 +0200

    update

--- T1/Proyectoinicial/src/Main.java
+++ T1/Proyectoinicial/src/Main.java
@@ -10,10 +10,19 @@ public class Main {
 
     public static void main(String[] args){
         System.out.print("Este es mi primer programa en java\n\n");
+        // La diferencia entre print y printnl es que en el segundo caso hay un salto de linea
         System.out.println("Este es la segunda linea del probrama");
         System.out.println("Esta es\nla tercera linea y estoy probando el salto de la n y del printnl a la vez dentro del parentesis");
         System.out.println("una pulga aventurera decidio salir de viaje");
-
+        String miNombre = "bryan";
+        miNombre = "Bryan T. Henao";
+        //para cambiarle el valor a la variable pones debajo el nombre de la variable sin el tipo y asi le cambias el valor
+        System.out.println("Mi nombre es: "+miNombre);
+        //justo aqui debajo pones un miNombre nuevo, y un system.out.println te sale el nuevo valor ej:
+        miNombre = "Bryan T.H";
+        System.out.println("Mi nombre es: "+miNombre);
+        char letraDNI = 'A';
+        System.out.println("La letra de mi DNI es: " +letraDNI);
     }
 
     public void nombreMetodo () {
